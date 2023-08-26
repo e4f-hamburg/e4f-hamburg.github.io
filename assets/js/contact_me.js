c4125b2070be66bea0b82b71dc364ac100c358c4
Corinna Hischke <corinna.hischke@infix.de>
2023-08-26 16:23:51 +0200

    Add form validation to contact form

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -1,24 +1,45 @@
 ---
 ---
-function contact_me() {
+$(function() {
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function contact_me() {
 
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
+     var name = $("input#name").val();
+     var email = $("input#email").val();
+     var phone = $("input#phone").val();
+     var message = $("textarea#message").val();
+     var firstName = name; // For Success/Failure Message
+     // Check for white space in name for Success/Fail message
+     if (firstName.indexOf(' ') >= 0) {
+       firstName = name.split(' ').slice(0, -1).join(' ');
+     }
 
-      message += "%0D%0A%0D%0AFreundliche Grüße,%0D%0A" + name + "%0D%0A" + email + "%0D%0A" + phone;
+     message += "%0D%0A%0D%0AFreundliche Grüße,%0D%0A" + name + "%0D%0A" + email + "%0D%0A" + phone;
 
-      // send message through href in button
-      document.location.href = "mailto:info@e4f-hamburg.de?subject=Kontaktanfrage an E4F&body=" + message;
+     // send message through href in button
+     document.location.href = "mailto:info@e4f-hamburg.de?subject=Kontaktanfrage an E4F&body=" + message;
 
-      // clear form fields
-      $("#contactForm").reset()
+     // clear form fields
+     $("#contactForm").reset()
 
-      return false;
-    }
+     return false;
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
