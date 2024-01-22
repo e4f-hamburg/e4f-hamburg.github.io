00363a1ab93985ddc2a6c8a0f14cf8fcf519b859
Martin Grotzke <martin.grotzke@inoio.de>
2024-01-22 10:39:23 +0100

    Detect browser navigation and close/open the modal
    This adds support for browser back/forward navigation to open/close
    events/modals.
    
    Without this, there might be unexpected behavior:
    * open the page
    * go to /zukunftsentscheid
    * go to events (via the menu)
    * open an event
    * navigate back (hit browser back button)
      * now the event/modal is still shown (unexpected?)
    * close the modal by hitting "x" or pressing ESC
      * now you'll land on /zukunftsentscheid again (unexpected)

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,6 +1,8 @@
 (function ($) {
 
     let modalClicked = false;
+    // ignoreEvent is used to prevent the hashchange or hidden.bs.modal event from triggering when we toggle or change the hash ourselves
+    let ignoreEvent = null;
 
     // on load, check if the window location hash links to a modal and show it - so that we have bookmarkable modals
     if (window.location.hash.startsWith("#event-")) {
@@ -12,19 +14,48 @@
     // we have bookmarkable urls for our modals
     $(document).on('click', '[data-toggle="modal"]', function (e) {
         e.preventDefault();
+        ignoreEvent = "hashchange";
         window.location.hash = this.hash;
         modalClicked = true;
     });
 
     // add event listener to reset the location.hash when a modal is closed
     $(document).on('hidden.bs.modal', function (e) {
+        if (ignoreEvent === "hidden.bs.modal") {
+            ignoreEvent = null;
+            return;
+        }
+
         // only go back in history if the modal was opened by clicking, and not e.g. because the modal
         // was shown because the user opened the bookmarkable url of the modal
+        ignoreEvent = "hashchange";
         if (modalClicked) {
             window.history.back();
         } else {
             // if the modal was opened by following a bookmarkable url, go to the portfolio section
             window.location.hash = "portfolio";
+        }
+    });
+
+    // detect browser back/forward navigation and close/open the modal accordingly
+    $(window).on('hashchange', function(e) {
+        if (ignoreEvent === "hashchange") {
+            ignoreEvent = null;
+            return;
+        }
+
+        // get location hash from previous page
+        let previousHash = e.originalEvent.oldURL.split("#")[1];
+        // get location hash from current page
+        let currentHash = e.originalEvent.newURL.split("#")[1];
+        // if the previous page was a modal, and the current page is not a modal, hide the modal
+        if (previousHash && previousHash?.startsWith("event-")) {
+            ignoreEvent = "hidden.bs.modal";
+            $('#' + previousHash).modal('toggle');
+        }
+        if (currentHash && currentHash.startsWith("event-")) {
+            ignoreEvent = "hidden.bs.modal";
+            $('#' + currentHash).modal('toggle');
         }
     });
 
